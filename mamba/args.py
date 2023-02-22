37643b6bad15d1dbdba24bda657166cd17112a36
nav1th <navith@keemail.me>
2023-02-22 13:44:35 +0000

    Added new option, and some other stuff

--- mamba/args.py
+++ mamba/args.py
@@ -10,13 +10,14 @@ def grab_args():
                         help="Select '.pcap' file to read packets from")
     parser.add_argument("-w","--write",required=False,default=False,
                         help="Writes packet capture to file")
-    parser.add_argument("-n","--non_promiscous",required=False, action="store_false",
+    parser.add_argument("-n","--non-promiscous",dest="non_promiscous",required=False, action="store_false",
                         help="Listen in non-promiscous mode, e.g only get packets addressed to your network interface")
     parser.add_argument("-f","--filter",required=False,type=str,help="Uses BPF syntax to filter packets")
-    parser.add_argument("-c","--count",required=False,type=int,default=0)
+    parser.add_argument("-c","--count",required=False,type=int,default=0,help="Show only the first [COUNT] amount of packets")
     parser.add_argument("-v","--verbose", required=False, action="store_true")
-    parser.add_argument("-cL","--colourless",required=False,action="store_false")
-    parser.add_argument("--no_confirm",required=False,action="store_true")
+    parser.add_argument("-cL","--colourless",required=False,action="store_false",help="Do not display coloured output")
+    parser.add_argument("--no-confirm",dest="no_confirm",required=False,action="store_true",help="Do not ask for confirmation in (y/N) prompts, (automatic yes)")
+    parser.add_argument("--show-raw",dest="show_raw",required=False,action="store_true") 
     args = parser.parse_args()
     if args.interface and args.read:
         m.err("can't read from 'pcap' and listen on interface at the same time",args.colourless)
