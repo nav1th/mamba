542a5efdcb2f1ba69b578051271b9de14a0a5401
nav1th <navith@keemail.me>
2023-02-22 13:59:07 +0000

    added pairs of different addresses

--- mamba/main.py
+++ mamba/main.py
@@ -13,7 +13,7 @@ from collections import Counter
 import os.path
 
 
-def good_colour_file(json) -> bool: #TODO check if correct colours are in colour file
+def good_colour_file(json) -> bool: 
     acceptable_colours = ["YELLOW", "RED", "BLUE", "CYAN", "MAGENTA", "GREEN", "WHITE", "BLACK"]
     for i in json['protocols']:
         if i['FG'] not in acceptable_colours:
@@ -22,17 +22,37 @@ def good_colour_file(json) -> bool: #TODO check if correct colours are in colour
             return False
     return True 
 
-
+def insert_src_dst_pairs(src, dst, counter):
+    key = tuple(sorted([src,dst])) #bundles src and dst together
+    counter.update(key) #updates amount of pairs 
 
 def proc_pkt(pkt): #handles packets depending on protocol
-    if layer2.ARP in pkt: #ARP 
+    ##possible address types
+    ether_src = None
+    ether_dst = None  
+    ip_src = None
+    ip_dst = None
+    ipv6_src = None
+    ipv6_dst = None
+    num_of_pkts = sum(packet_count)
+
+    if layer2.Ether in pkt:  ## grab ethernet info if any
         ether_src = pkt[layer2.Ether].src 
         ether_dst = pkt[layer2.Ether].dst  
-        arp = pkt[layer2.ARP] 
+        insert_src_dst_pairs(ether_src,ether_dst,pairs_ether)
+        
+    if inet.IP in pkt: # grab ipv4 info if any
+        ip_src = pkt[inet.IP].src
+        ip_dst = pkt[inet.IP].dst
+        insert_src_dst_pairs(ip_src,ip_dst,pairs_ipv4)
+       
+    if inet6.IPv6 in pkt: # grab ipv6 info if any
+        ipv6_src = pkt[inet6.IPv6].src
+        ipv6_dst = pkt[inet6.IPv6].dst
+        insert_src_dst_pairs(ip_src,ip_dst,pairs_ipv6)
 
-        key = tuple(sorted([ether_src, ether_dst])) #bundles ether src and dst together
-        packet_count.update(key) #updates packet count
-        num_of_pkts = int(sum(packet_count.values()) / 2)
+    if layer2.ARP in pkt: #ARP       
+        arp = pkt[layer2.ARP] 
         if colour:
             if colour_json:
                 ARP_fg = "json magic fg"
@@ -51,18 +71,12 @@ def proc_pkt(pkt): #handles packets depending on protocol
                 print(f"ARP: {arp.psrc} is asking who has MAC for {arp.pdst}")
             elif arp.op == 2: #ARP I'm your man here's your MAC
                 print(f"ARP: {arp.hwsrc} is at {arp.psrc}")
-    if layer2.Neighbor in pkt:
-        ether_src = pkt[layer2.Ether].src 
-        ether_dst = pkt[layer2.Ether].dst  
-        neighbour = pkt[layer2.Neighbor]
-        
-        pass
-    
-#TODO HTTP Request
-    if pkt.haslayer(scapy.layers.http.HTTPRequest): 
+                
+    if inet6.ICMPv6ND_RS in pkt: #discover routers on Ipv6 network with all routers multicast ff02::2
+        rs = pkt[inet6.ICMPv6ND_RS]
+        print(f"{num_of_pkts} | {ipv6_src} ==> {ipv6_dst}", end=" | ")
+    if scapy.layers.http.HTTPRequest in pkt: # HTTP Request
         req = pkt[scapy.layers.http.HTTPRequest]
-        ip_src = pkt[inet.IP].src
-        ip_dst = pkt[inet.IP].dst
         sport = pkt[inet.TCP].sport
         dport = pkt[inet.TCP].dport
 
@@ -81,8 +95,7 @@ def proc_pkt(pkt): #handles packets depending on protocol
 #TODO TELNET
 #TODO FTP
 #TODO OTHER PROTOCOLS ALONG DE WAY
-
-
+    #packet_count.update([pkt])
         
 if __name__ == "__main__":
     args = args.grab_args() #grab arguments from CLI input
@@ -132,6 +145,9 @@ if __name__ == "__main__":
 
 
     packet_count = Counter() # count the number of packets captured
+    pairs_ipv4 = Counter() 
+    pairs_ipv6 = Counter()
+    pairs_ether = Counter()
 
     if read_pcap:
         try: 
