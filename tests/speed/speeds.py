fd2ab2483d8f906fdbabbed28d5f62095b051e6f
nav1th <navith@keemail.me>
2023-05-04 12:50:21 +0100

    will get averages from 2 devices

--- tests/speed/speeds.py
+++ tests/speed/speeds.py
@@ -1,4 +1,5 @@
 from itertools import cycle
+import sys
 from d1 import D1
 from d2 import D2
 
@@ -11,15 +12,25 @@ def mean(*nums: list[float]):
         return means[0]
     return means
 
+prog_index = ([
+                ("mamba",0),
+                ("tcpdump",1),
+                ("tshark",2)],
+                "572kb"),([("mamba",3),("tcpdump",4),("tshark",5)],"18mb")
 
-prog_index = cycle([("mamba", 0), ("tcpdump", 1), ("tshark", 2)])
-for avrg in D1:
-    print
+for f_size_tests in prog_index:
+    print(f_size_tests[1])
+    for prog in f_size_tests:  
     means_572kb = mean(avrg)
+    
 means_18mb = mean(mamba_speeds_18mb, tcpdump_speeds_18mb, tshark_speeds_18mb)
+
 for x, y in prog_index:
     print(f"{x}: {means_572kb[y]} secs")
 
 print("\n18mb pcap\n")
 for x, y in prog_index:
     print(f"{x}: {means_18mb[y]} secs")
+
+if sys.platform == "win32":
+    input("press enter to exit...")
